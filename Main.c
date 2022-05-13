6ff9daea9f707944581085594d44c59f0911e5ad
nerusskyhigh <Guglielmo.grillo@gmail.com>
2022-05-13 16:26:14 +0200

    first working draft

--- Main.c
+++ Main.c
@@ -1,37 +1,39 @@
 #include<stdio.h>
-
+#include<math.h>
 #include <time.h>
 #include <stdlib.h>
+#include<string.h>
 
-#include<stdbool.h> 
-
-#define sgn(x)( x>=0 ? +1 : -1)
+#define N_FLIPS_PER_STEP 1
+#define N_MARKOV_STEPS 1000
 
 /*
  * Generates the initial configuration.
  *  Should it be random?
  */
-void init(bool *lattice, const int L)
+void init(int *lattice, const int L)
 {
-    double r;
+    int r;
     for(int row=0; row<L; row++)
     {
-        for(int col; col<L; col++)
+        for(int col=0; col<L; col++)
         {
-            r = rand()/RAND_MAX;
-            lattice[row*L + col] = sgn(r-0.5);
+            r = rand() % 2; // Returns 0 or 1
+            //printf("%d", r ? 1 : -1);
+            lattice[row*L + col] = (r ? 1 : -1);
         }
+        //printf("\n");
     }
 }
 
 /*
  * Copies the lattice into an other lattice
  */
-void copy(bool *dest_lattice, const bool *src_lattice, const int L)
+void copy(int *dest_lattice, const int *src_lattice, const int L)
 {
     for(int row=0; row<L; row++)
     {
-        for(int col; col<L; col++)
+        for(int col=0; col<L; col++)
         {
             dest_lattice[row*L + col] = src_lattice[row*L + col];
         }
@@ -51,8 +53,9 @@ int computeEnergy(const int *lattice, int L)
         idx_b; // and the one below
     for(int row=0; row<L; row++)
     {
-        for(int col; col<L; col++) // No PBC
+        for(int col=0; col<L; col++)
         {
+            // Periodic Boundary Conditions
             idx = row*L + col;
             idx_r = (col == L-1) ? row*L : row*L + col+1;
             idx_b = (row == L-1) ? col   : (row+1)*L + col;
@@ -76,11 +79,11 @@ int propose_configuration(int *lattice, int n_flips, int L, int E)
     int row, col,
         idx_l, idx_a, idx_r, idx_b; //Lefct, above, right, below
 
-
     for(int n=0; n<n_flips; n++)
     {
-        row = rand()/RAND_MAX * L;
-        col = rand()/RAND_MAX * L;
+        row = rand()%L;
+        col = rand()%L;
+        //printf("row=%d\t col=%d\n", row, col);
 
         lattice[row*L+col] = -1*lattice[row*L+col];
 
@@ -104,78 +107,129 @@ int propose_configuration(int *lattice, int n_flips, int L, int E)
  */
 int montecarlo_step(int *lattice, int L, int E, double T)
 {
-    int target; // Lattice site to flip
-    bool **lattice_prop = (bool**) calloc(L*L, sizeof(bool*));
+    int E_prop; // Energy of the proposed configuration
+    int *lattice_prop = (int*) calloc(L*L, sizeof(int*));
 
     copy(lattice_prop, lattice, L);
-    // NUMBER OF FLIPS TO BE DETERMINED
-    // !!!
-    E_prop = propose_configuration(lattice_prop, 1, L, E);
+    E_prop = propose_configuration(lattice_prop, N_FLIPS_PER_STEP, L, E);
 
     // Acceptation step
-    double w = exp(- (E_prop-E)/T)
-    double r = rand()/RAND_MAX;
+    double w = exp(- (E_prop-E)/T);
+    double r = 1.0*rand()/RAND_MAX;
+    //printf("E_prop=%d\t w=%f\t r=%f\t T=%f\n",E_prop, w, r, T);
+
     if(r <= w) {
         // Accepted
-        lattice = lattice_prop;
+        copy(lattice, lattice_prop, L);
+        free(lattice_prop);
         return 1;
     } else {
+        free(lattice_prop);
         return 0; // Rejected
     }
 
 }
 
-void printLattice(const int *lattice, const int size)
+/*
+ * Prints the lattice.
+ *      spin=1 --> +
+ *      spin=0 --> O
+ */
+void printLattice(const int *lattice, const int L)
 {
-    for(int row=0; row<size; row++)
+    for(int row=0; row<L; row++)
     {
-        for(int col; col<size; col++)
+        for(int col=0; col<L; col++)
         {
-            if( lattice[row*size+col] == 1) {
-                printf("%c", (char)254u);
+            if( lattice[row*L+col] == 1) {
+                printf("+");
             } else {
-                printf("  ");
+                printf("O");
             }
         }
-        printf("\n")
+        printf("\n");
     }
 }
+
+/*
+ * Save ising lattice to file in CSV format
+ * The columns are, in order,
+ *      Lattice size, Temperature, Lattice Energy, lattice sites
+ */
+void save(FILE *fptr, int L, double T, int E, int *lattice)
+{
+    fprintf(fptr,"%d, %lf, %d", L, T, E);
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            fprintf(fptr, ", %d", lattice[row*L+col]);
+        }
+    }
+    fprintf(fptr, "\n");
+}
+
 
 /*
  * The aim of this program is to generate Ising Configurtions
  * on a lattice
  */
-int main(void)
+int main(int argc, char *argv[])
 {
     int L, E;
     double T;
+    FILE *fptr;
 
     // Parse command line arguments
     for (int i = 0; i < argc; i++) {
         if( strcmp(argv[i], "-L")==0 &&  i+1<argc ) {
             L = atoi(argv[i+1]); // Side of the lattice
         } else if( strcmp(argv[i], "-T")==0 &&  i+1<argc ) {
-            T = atoi(argv[i+1]); // Temperature
+            T = atof(argv[i+1]); // Temperature
+        } else if( strcmp(argv[i], "-o")==0 &&  i+1<argc ) {
+            fptr = fopen(argv[i+1], "w");
+            if(fptr == NULL) {
+                printf("Can not open file %s\n", argv[i+1]);
+                exit(1);
+            }
         }
     }
-
-
-    bool **lattice = (bool**) calloc(L*L, sizeof(bool*));
     
+    int *lattice = (int*) calloc(L*L, sizeof(int));
+
     srand(time(NULL));
 
-    for(int i = 0; i< 100; i++) {
-        // Generate random initial configuration
-        init(lattice, L);
+    // Generate random initial configuration
+    printf("init:\n");
+    init(lattice, L);
 
+
+    int acc, steps;
+    for(int i = 0; i<N_MARKOV_STEPS; i++) {
+
+        //printf("computeEnergy: ");
         E = computeEnergy(lattice, L);
+        //printf("%d\n", E);
 
-        montecarlo_step(lattice, L, E, T)
+        acc = 0;
+        steps = 0;
+        while( steps < 1000) {
+            steps++;
+            acc+=montecarlo_step(lattice, L, E, T);
+            //printf("%f\t", (double) acc/steps );
+        }
 
-        printLattice(lattice);
-        printf("\n\n");
+        montecarlo_step(lattice, L, E, T);
+        E = computeEnergy(lattice, L);
+        save(fptr, L, T, E, lattice);
+
+        printf("montecarlo_step: n=%d/%d=%f\t E=%d\t T=%f\n", acc, steps, (double) acc/steps, E, T);
+
+        /*
+            printf("printLattice:\n");
+            printLattice(lattice, L);
+            printf("\n----------------------------------\n");
+         */
     }
 
+    fclose(fptr);
     return 0;
 }
-
