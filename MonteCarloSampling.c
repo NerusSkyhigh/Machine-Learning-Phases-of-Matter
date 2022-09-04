7f6238a147ac8c862ee1436ef7e4c53361f92ed4
nerussyhigh <Guglielmo.grillo@gmail.com>
2022-09-04 20:22:04 +0200

    back to cold init

--- MonteCarloSampling.c
+++ MonteCarloSampling.c
@@ -5,7 +5,7 @@
 #include<string.h>
 
 #define N_FLIPS_PER_STEP 1
-#define HOT_INIT 1
+#define HOT_INIT 0
 
 // steps: number of steps since last valid configuration
 // acc: number of accepted configurations
